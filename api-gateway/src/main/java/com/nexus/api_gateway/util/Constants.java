70114d18179bb59d21c906e65e7fe592d9ff5f3c
Mohd Ataa Khan <trendsandfactss@gmail.com>
2025-11-21 21:46:11 +0530

    Adding Api gateway

---
+++ api-gateway/src/main/java/com/nexus/api_gateway/util/Constants.java
@@ -0,0 +1,6 @@
+package com.nexus.api_gateway.util;
+
+public final class Constants {
+    public static final String USER_SERVICE_URI = "http://localhost:3000/api/v1/auth/user/validate";
+    public static final String USER_ID_HEADER = "X-User-ID";
+}
